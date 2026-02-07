426e34ed726183bb4d22354cf0ab5c5c1441ae2d
SeolYoungKim <nasur4da@gmail.com>
2026-02-07 21:00:13 +0900

    refactor: 메서드 추출

--- syw/src/main/java/c4/mystorage/presentation/StorageController.java
+++ syw/src/main/java/c4/mystorage/presentation/StorageController.java
@@ -3,6 +3,7 @@ package c4.mystorage.presentation;
 import c4.mystorage.application.StorageItemCreate;
 import c4.mystorage.application.StorageService;
 import org.apache.tika.Tika;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 public class StorageController {
@@ -22,7 +24,7 @@ public class StorageController {
         this.storageService = storageService;
     }
 
-    @PostMapping(path = "/storage-items", consumes = "multipart/form-data")
+    @PostMapping(path = "/storage-items", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> save(
             @RequestHeader("X-OWNER-ID") Long ownerId,
             @RequestPart MultipartFile file,
@@ -36,10 +38,16 @@ public class StorageController {
                         file.getInputStream(),
                         file.getSize(),
                         payload.itemType(),
-                        tika.detect(file.getInputStream()),
+                        detectContentType(file),
                         payload.extraMetadata()
                 )
         );
         return ResponseEntity.ok().build();
+    }
+
+    private String detectContentType(MultipartFile file) throws IOException {
+        try (InputStream inputStream = file.getInputStream()) {
+            return tika.detect(inputStream, file.getOriginalFilename());
+        }
     }
 }
