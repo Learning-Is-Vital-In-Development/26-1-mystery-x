0d092010e4c8504c7d83f354aebc7ebc8a5fe03a
SeolYoungKim <nasur4da@gmail.com>
2026-02-07 21:16:54 +0900

    feat: 파일 다운로드 API 구현

--- syw/src/main/java/c4/mystorage/presentation/StorageController.java
+++ syw/src/main/java/c4/mystorage/presentation/StorageController.java
@@ -1,18 +1,21 @@
 package c4.mystorage.presentation;
 
+import c4.mystorage.application.StorageFileData;
 import c4.mystorage.application.StorageItemCreate;
 import c4.mystorage.application.StorageService;
 import org.apache.tika.Tika;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 public class StorageController {
@@ -49,5 +52,22 @@ public class StorageController {
         try (InputStream inputStream = file.getInputStream()) {
             return tika.detect(inputStream, file.getOriginalFilename());
         }
+    }
+
+    @GetMapping("/storage-items/{storedName}")
+    public ResponseEntity<Resource> download(
+            @RequestHeader("X-OWNER-ID") Long ownerId,
+            @PathVariable String storedName
+    ) {
+        StorageFileData fileData = storageService.getFile(ownerId, storedName);
+        ContentDisposition disposition = ContentDisposition.attachment()
+                .filename(fileData.displayName(), StandardCharsets.UTF_8)
+                .build();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(fileData.contentType()))
+                .contentLength(fileData.file().length())
+                .header(HttpHeaders.CONTENT_DISPOSITION, disposition.toString())
+                .body(new FileSystemResource(fileData.file()));
     }
 }
